28dc6895f3acb5eed3ef0163260d83af3304c7df
Neiro Somemiya <smmy@neiro.work>
2020-07-18 08:34:35 +0900

    hello express

--- .dist/index.js
+++ .dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Logger {
+    constructor(o) {
+        this.app = express_1.default();
+        this.port = o.port;
+    }
+    start() {
+        this._indexHandler();
+        this.app.listen(this.port, () => console.log('ready'));
+    }
+    _indexHandler() {
+        this.app.get('/', (req, res) => {
+            console.log(req, res);
+            return res.send('hello world!');
+        });
+    }
+}
+const logger = new Logger({ port: 3000 });
+logger.start();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello world')
+import express from 'express';
+
+class Logger {
+  private app: express.Application;
+  private port: number;
+
+  constructor(o: { port: number }) {
+    this.app = express();
+    this.port = o.port;
+  }
+
+  start(): void {
+    this._indexHandler();
+
+    this.app.listen(this.port, () => console.log('ready'));
+  }
+
+  private _indexHandler(): void {
+    this.app.get('/', (req: express.Request, res: express.Response) => {
+      console.log(req, res);
+      return res.send('hello world!');
+    });
+  }
+}
+
+const logger = new Logger({ port: 3000 });
+logger.start();
